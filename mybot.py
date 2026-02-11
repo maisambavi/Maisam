e2cc6dee5a5041d385713cc3996d8c9d5111c50c
maisambavi <maisambavi01@gmail.com>
2026-02-11 23:49:44 +0330

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,19 +1,18 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8321454833:AAFwZ4-z-4EdQM8_8PxbT7QXtpOFDXU5zKw"
+WEBHOOK_URL = "https://maisam-4wh0.onrender.com/webhook"  # لینک سرویس Render شما + /webhook
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("به ربات میثم خوش آمدید")
+    await update.message.reply_text("به ربات میثم خوش آمدید!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-PORT = int(os.environ.get("PORT", 10000))
-
+# این قسمت Webhook رو فعال می‌کنه
 app.run_webhook(
     listen="0.0.0.0",
-    port=PORT,
-    webhook_url="https://maisam-4wh0.onrender.com"
+    port=10000,
+    webhook_url=WEBHOOK_URL
 )
