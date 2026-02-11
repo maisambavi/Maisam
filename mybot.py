d7b1b2d0e3beeaf30ccf8c2c3e2bf275f0af07a1
maisambavi <maisambavi01@gmail.com>
2026-02-11 23:43:46 +0330

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,10 +1,19 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("به ربات میثم خوش امدید ")
-app = ApplicationBuilder().token("8321454833:AAFwZ4-z-4EdQM8_8PxbT7QXtpOFDXU5zKw").build()
+TOKEN = "8321454833:AAFwZ4-z-4EdQM8_8PxbT7QXtpOFDXU5zKw"
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("به ربات میثم خوش آمدید")
+
+app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+PORT = int(os.environ.get("PORT", 10000))
+
+app.run_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    webhook_url="https://maisam-4wh0.onrender.com"
+)
